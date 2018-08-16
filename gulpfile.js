f5d7bdc1461ad4efe383717ffa2d8ca6d9a3c0ba
Abhinav Pandey <abhinav.05.pandey@gmail.com>
2018-08-16 19:31:22 +0530

    /public renamed to /src

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,20 @@ var browserSync = require('browser-sync').create();
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
-            baseDir: 'public'
+            baseDir: 'src'
         },
     })
 });
 
 gulp.task('watch', ['browserSync'], function (){
-    gulp.watch('public/*.html', browserSync.reload); 
-    gulp.watch('public/js/**/*.js', browserSync.reload); 
-    gulp.watch('public/components/**/*.handlebars', browserSync.reload); 
+    gulp.watch('src/*.html', browserSync.reload); 
+    gulp.watch('src/js/**/*.js', browserSync.reload); 
+    gulp.watch('src/components/**/*.handlebars', browserSync.reload); 
 });
 
 var useref = require('gulp-useref');
 gulp.task('useref', function() {
-    return gulp.src('public/*.html')
+    return gulp.src('src/*.html')
       .pipe(useref())
       .pipe(gulp.dest('dist'))
   });
@@ -26,7 +26,7 @@ gulp.task('useref', function() {
   var gulpIf = require('gulp-if');
   
   gulp.task('useref', function(){
-    return gulp.src('public/*.html')
+    return gulp.src('src/*.html')
       .pipe(useref())
       // Minifies only if it's a JavaScript file
       .pipe(gulpIf('*.js', uglify()))
@@ -35,7 +35,7 @@ gulp.task('useref', function() {
 
 var cssnano = require('gulp-cssnano');
 gulp.task('useref', function(){
-  return gulp.src('public/*.html')
+  return gulp.src('src/*.html')
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
     // Minifies only if it's a CSS file
@@ -45,14 +45,14 @@ gulp.task('useref', function(){
 
 var imagemin = require('gulp-imagemin');
 gulp.task('images', function(){
-    return gulp.src('public/assets/**/*.+(png|jpg|gif|svg)')
+    return gulp.src('src/assets/**/*.+(png|jpg|gif|svg)')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images'))
 });
 
 var cache = require('gulp-cache');
 gulp.task('images', function(){
-    return gulp.src('public/assets/**/*.+(png|jpg|gif|svg)')
+    return gulp.src('src/assets/**/*.+(png|jpg|gif|svg)')
     // Caching images that ran through imagemin
     .pipe(cache(imagemin({
       interlaced: true
